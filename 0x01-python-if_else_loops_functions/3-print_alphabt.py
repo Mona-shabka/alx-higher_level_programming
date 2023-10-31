1409ce62acfff26c4d42ce1cb6dc77648e646b35
Mona-shabka <mona.shabka@yahoo.com>
2023-10-31 12:45:13 -0700

    1

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for n in range(ord('a'), ord('z') + 1):
-	if n != ord('q') and n != ord('e'):
-		print("{:c}".format(n), end="")
+    if n != ord('q') and n != ord('e'):
+        print("{:c}".format(n), end="")
