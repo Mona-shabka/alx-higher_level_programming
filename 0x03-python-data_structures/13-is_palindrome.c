c3d7c19c94b4bbc8b80a15931371490e05e64dd8
Mona-shabka <mona.shabka@yahoo.com>
2023-11-04 17:42:34 -0700

    1

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - a function that check palindrome.
+ * @head: list head.
+ * Return: 0 (not a palindrome), 1 (palindrome).
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (aux_palind(head, *head));
+}
+
+/**
+ * aux_palind - a function that checks palindrome.
+ * @head: list head.
+ * @end: list end.
+ * Return: nothing.
+ */
+
+int aux_palind(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+		return (1);
+	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
