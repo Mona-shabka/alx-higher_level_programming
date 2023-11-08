9a2f37679d2e9626b0edeb313d1487463a91fc02
Mona-shabka <mona.shabka@yahoo.com>
2023-11-08 05:31:10 -0800

    1

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     for m in list(a_dictionary.keys()):
-        if a_dictionary[key] == value:
-            del a_dictionary[key]
+        if a_dictionary[m] == value:
+            del a_dictionary[m]
     return (a_dictionary)
