19f3dedcfc03e32f017ef30b164c17675b27c45a
Mona-shabka <mona.shabka@yahoo.com>
2023-11-20 05:44:08 -0800

    1

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for m in range(0, list_length):
+    for m in range(list_length):
         try:
             d = my_list_1[m] / my_list_2[m]
         except TypeError:
