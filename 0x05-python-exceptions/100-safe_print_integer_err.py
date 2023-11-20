cb061612753704b2fd7134bed6353c9ad6688658
Mona-shabka <mona.shabka@yahoo.com>
2023-11-20 05:54:27 -0800

    1

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return (True)
+        return True
     except Exception as m:
         print("Exception:", m, file=sys.stderr)
-        return (False)
+        return False
