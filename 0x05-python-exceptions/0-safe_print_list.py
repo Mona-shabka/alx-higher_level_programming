9ab59e7829ad7d06fe764a6a98e99aa160f0b6f6
Mona-shabka <mona.shabka@yahoo.com>
2023-11-20 05:24:10 -0800

    1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     m = 0
     try:
         while m is not x:
-            print(my_list[m], end="")
+            print(my_list[m], end='')
             m += 1
     except IndexError:
         None
