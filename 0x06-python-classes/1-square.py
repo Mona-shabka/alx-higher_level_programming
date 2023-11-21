c7efc0eee6f6d206d45352112af45ff95d2c7e0d
Mona-shabka <mona.shabka@yahoo.com>
2023-11-21 04:20:09 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""square module """
+
+
+class Square:
+    """Define square"""
+
+    def __init__(self, size):
+        """square
+        Args: size - represnets square size.
+        """
+        self.__size = size
