aaa87b7ccb25eea5f5c52f2c255b75a6ef8767f3
Mona-shabka <mona.shabka@yahoo.com>
2023-12-04 07:50:30 -0800

    1

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This module backs the list of available attributes
+    and methods of an object.
+"""
+
+
+def lookup(obj):
+    """Functions uses for all attributes and methods of an object"""
+    return dir(obj)
