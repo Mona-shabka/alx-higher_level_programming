d6ea1ef23671ec15b4bc9d2b95387ec8c7c6de9a
Mona-shabka <mona.shabka@yahoo.com>
2024-03-26 04:19:48 -0700

    1

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,23 @@
 #!/usr/bin/node
-
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
+request.get('http://swapi.co/api/films/' + process.argv[2], (err, resp, body) => {
+  if (err) console.log(err);
+  else if (resp.statusCode === 200) {
+    let film = JSON.parse(body);
+    let promises = [];
+    for (let ch of film.characters) {
+      promises.push(new Promise((resolve, reject) => {
+        request.get(ch, (err, resp, body) => {
+          if (err) {
+            reject(err);
+          } else if (resp.statusCode === 200) {
+            resolve(JSON.parse(body).name);
+          } else {
+            reject(Error('Unknown'));
+          }
+        });
+      }));
     }
+    Promise.all(promises).then((names) => names.forEach((name) => console.log(name)));
   }
 });
